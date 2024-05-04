f2df4e8db7182da3201168af44edcd4f4e5db37e
Jarek Jeda <jarek.jeda@gmail.com>
2024-05-04 17:14:51 +0200

    adding works

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,20 @@
 #pragma once
+#include <iostream>
+#include <cstdlib>
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+	while(rhs != 0) {
+	int temp = rhs;
+	rhs = lhs % rhs;
+	lhs = temp;
+	}
+
+	return abs(lhs);
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+	if(NWD(lhs, rhs) == 0) {
+		return 0;
+	}
+	return abs((lhs * rhs) / NWD(lhs, rhs));
 }
