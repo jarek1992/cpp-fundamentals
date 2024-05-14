ccd2b820c1497870cf43b271aea712599ba7491b
Jarek Jeda <jarek.jeda@gmail.com>
2024-05-14 18:51:43 +0200

    vector-of-shared-ptrs

--- homework/vector-of-shared-ptrs/vectorFunctions.cpp
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -3,7 +3,7 @@
 std::vector<std::shared_ptr<int>> generate(int count) {
 	std::vector<std::shared_ptr<int>> vec;
 	for(int i = 0; i < count; ++i) {
-		vec.push_back(std::make_shared(i));
+		vec.push_back(std::make_shared<int>(i));
 	}
 	return vec;
 }
@@ -17,20 +17,20 @@ void print(std::vector<std::shared_ptr<int>>& vec) {
 
 void add10(std::vector<std::shared_ptr<int>>& vec) {
 	for(auto& element : vec) {
-		*element += 10;
+		if(element != nullptr) {
+			*element += 10;
+		}
 	}
-}
 
-void sub10(int* ptr) {
+void sub10(int* const ptr) {
 	if(ptr != nullptr) {
 		*ptr -= 10;
 	}
 }
 
 void sub10(std::vector<std::shared_ptr<int>>& vec) {
-	for(const auto& ptr :vec) {
-		sub10(ptr.get());
+	for(const auto& element :vec) {
+		sub10(element.get());
 	}
 }
-
 
